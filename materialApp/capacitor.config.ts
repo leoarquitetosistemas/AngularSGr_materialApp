d40c275880dfdb82bc320a4db0f34517e119d05b
leonardo.conrado <leonardo.conrado@fgv.br>
2022-09-19 12:14:04 -0300

    Angular Material template convertido em aplicação ANdroid, iOS e electron via capacitor

---
+++ materialApp/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.SimonGrimmMaterialApp',
+  appName: 'material-app',
+  webDir: 'dist/material-app',
+  bundledWebRuntime: false
+};
+
+export default config;
